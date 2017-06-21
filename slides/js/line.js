3d4df6ae9945a3d54e4dc1c65524bc94742f0662
Matthew Would <matthew.would@derivco.co.za>
2017-06-21 10:19:48 +0200

    new slides

--- slides/js/line.js
+++ slides/js/line.js
@@ -1,28 +1,73 @@
-function Terrain(detail) {
+function Fractal(detail) {
     this.size = Math.pow(2, detail) + 1;
     this.lineWidth = this.size - 1;
     this.map = new Float32Array(this.size);
 }
 
-Terrain.prototype.get = function (x) {
+Fractal.prototype.get = function (x) {
     if (x < 0 || x > this.lineWidth) return -1;
     return this.map[x];
 };
 
-Terrain.prototype.set = function (x, val) {
+Fractal.prototype.set = function (x, val) {
     this.map[x] = val;
-    // console.log(`${x},    this.map[${x}] = ${val}`);
+    console.log(`${x},    this.map[${x}] = ${val}`);
 };
 
-Terrain.prototype.generate = function (roughness) {
+Fractal.prototype.draw = function (renderer, scene, camera, width) {
     const self = this;
+
+    const material = new THREE.LineBasicMaterial({color: 0x00ff00}),
+        geometry = getGeometry(width),
+        line = new THREE.Line(geometry, material);
+
+    scene.add(line);
+    renderer.render(scene, camera);
+
+
+    function getGeometry(width) {
+        const geometry = new THREE.Geometry(),
+            scale = width / self.lineWidth;
+
+        for (let x = 0; x <= self.lineWidth; x++) {
+            const val = self.get(x);
+            geometry.vertices.push(new THREE.Vector3(x * scale, val * 20, 0));
+        }
+
+        return geometry;
+    }
+};
+
+function getCamera(width, height) {
+    const camera = new THREE.PerspectiveCamera(27, width / height, 1, 10000);
+    camera.position.set((width / 2), 0, 1000);
+    return camera;
+}
+
+function getLight() {
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(50, 250, 500);
+    return light;
+}
+function getRenderer(width, height) {
+    const renderer = new THREE.WebGLRenderer({canvas: document.getElementById('scene')});
+    renderer.setClearColor(0x3F3F3F);
+    renderer.setSize(width, height);
+    return renderer;
+}
+
+function clear() {
+    while (scene.children.length > 0) {
+        scene.remove(scene.children[0]);
+    }
+}
+
+Fractal.prototype.generate = function (roughness) {
+    const self = this;
+    roughness = roughness / (this.size / 20);
 
     this.set(0, 0);
     this.set(this.lineWidth, 0);
-
-    roughness = roughness / (this.size / 20);
-
-    // console.log(`---------------------------------------`);
 
     divide(this.lineWidth);
 
@@ -37,7 +82,6 @@ Terrain.prototype.generate = function (roughness) {
                 average = getAverage(x, halfLineWidth);
             self.set(x, average + offset);
         }
-
         divide(halfLineWidth);
     }
 
@@ -47,58 +91,12 @@ Terrain.prototype.generate = function (roughness) {
     }
 };
 
-Terrain.prototype.draw = function (renderer, scene, camera, width) {
-    const self = this;
-
-    const material = new THREE.LineBasicMaterial({color: 0x00ff00}),
-        geometry = getGeometry(width),
-        line = new THREE.Line(geometry, material);
-
-    scene.add(line);
-    renderer.render(scene, camera);
-
-
-    function getGeometry(width) {
-        const geometry = new THREE.Geometry(),
-            scale = width / self.size;
-
-        for (let x = 0; x <= self.size; x++) {
-            const val = self.get(x);
-            geometry.vertices.push(new THREE.Vector3(x * scale, val * 20, 0));
-        }
-
-        return geometry;
-    }
-};
-
-function getCamera(width, height) {
-    const camera = new THREE.PerspectiveCamera(50, width / height, 1, 10000);
-    camera.position.set((width / 2) - 50, 0, 1000);
-    return camera;
-}
-function getLight() {
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(50, 250, 500);
-    return light;
-}
-
-function getRenderer(width, height) {
-    const renderer = new THREE.WebGLRenderer({canvas: document.getElementById('scene')});
-    renderer.setClearColor(0x3F3F3F);
-    renderer.setSize(width, height);
-    return renderer;
-}
-
-function clear(){
-    while(scene.children.length > 0){
-        scene.remove(scene.children[0]);
-    }
-}
-
 function init(detail = 3, roughness = 0.5) {
     clear();
 
-    const terrain = new Terrain(detail); // 3, 5, 9, 17, 33, 65, 129, 257 ...
+    console.log('---- init ----');
+
+    const terrain = new Fractal(detail); // 3, 5, 9, 17, 33, 65, 129, 257 ...
     terrain.generate(roughness);
     terrain.draw(renderer, scene, camera, width);
 }
@@ -112,10 +110,9 @@ const width = window.innerWidth - 20,
 scene.add(getLight());
 scene.add(camera);
 
-window.addEventListener('mousemove', function(e) {
+window.addEventListener('mousemove', function (e) {
     const detail = Math.round(e.clientX / 150),
         roughness = e.clientY / 500;
-    console.log(`detail ${detail} roughness ${roughness}`);
 
     init(detail, roughness);
 });
